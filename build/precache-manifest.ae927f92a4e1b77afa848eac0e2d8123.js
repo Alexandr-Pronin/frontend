c66dd189a78230671b183c210664b398eb6dbb16
Alexander Pronin <aleksandr.pronin@htw-dresden.de>
2021-05-28 19:36:37 +0200

    build

---
+++ build/precache-manifest.ae927f92a4e1b77afa848eac0e2d8123.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2232d223611def12e491ffc24567e2f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "036585ff58d1e936adbc",
+    "url": "/static/js/2.0b61ad63.chunk.js"
+  },
+  {
+    "revision": "b29488b95d9a22d4e5809dc274ecb800",
+    "url": "/static/js/2.0b61ad63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a51d1b799ef1d66d7a70",
+    "url": "/static/js/3.7b5a6b2e.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/3.7b5a6b2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a4c281925ff41ff4250",
+    "url": "/static/js/4.4413f924.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/static/js/4.4413f924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e568ddcb50d62768a49",
+    "url": "/static/js/5.28f2594d.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.28f2594d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82d7a49d1f2427296004",
+    "url": "/static/js/main.ab107a18.chunk.js"
+  },
+  {
+    "revision": "2238c9a71d2730f2ded1",
+    "url": "/static/js/runtime-main.9c0df964.js"
+  }
+]);
